97799878a4e0cb5dfcf128af8db8ec6f938de185
Devilpermeet <hhahahmom@gmail.com>
2024-10-01 13:09:25 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const { Client, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const config = require('./config/config.json');
+
+const client = new Client();
+client.commands = new Collection();
+
+// Load commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Load events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(file.split('.')[0], event.bind(null, client));
+}
+
+// Connect to MongoDB
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Login the bot
+client.login(config.token);
